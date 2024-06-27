808616d82d9663482d28ad95588cdb6dfc79d6b5
Sachin Mali <115544254+Sachin015@users.noreply.github.com>
2024-06-27 09:08:06 +0545

    Add files via upload

---
+++ timeMethods/setTimeout_clearTimeout.js
@@ -0,0 +1,24 @@
+//setTimeout Metho() Execute the function after completing the given time
+
+//1)normal function
+function sayHello() {
+    console.log("Hello, world!");
+}
+
+// Call sayHello after 1000 milliseconds (1 second)
+let timeoutId = setTimeout(sayHello, 1000);
+console.log(timeoutId)
+
+// To cancel the timeout, you can use clearTimeout
+// clearTimeout(timeoutId);
+
+//2) using SetTimeout direct with call back function
+const exe = setTimeout(()=> {
+    console.log('hello')
+},1000)
+
+console.log(exe);
+
+
+//clear time
+clearTimeout(exe);
